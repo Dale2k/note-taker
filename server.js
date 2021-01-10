2580cee4a4f5a48e09c5c6c8e689cfa52b84609d
Darryl Subal <dsubal@att.net>
2021-01-10 12:26:52 -0500

    update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//static files css
+app.use(express.static(__dirname));
 
 
 
